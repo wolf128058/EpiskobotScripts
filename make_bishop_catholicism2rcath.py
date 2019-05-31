74555fb6b271250f5ee8626b01a9ae1fea32f556
Jonas Hess <jonas.hess@mailbox.org>
2019-05-31 23:07:49 +0200

    ++ new scripts converting catholicism to roman-catholic church

---
+++ make_bishop_catholicism2rcath.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import pywikibot
+from pywikibot import pagegenerators as pg
+
+with open('bishop_catholizism.rq', 'r') as query_file:
+    QUERY = query_file.read()
+
+wikidata_site = pywikibot.Site('wikidata', 'wikidata')
+
+# print(QUERY)
+
+generator = pg.WikidataSPARQLPageGenerator(QUERY, site=wikidata_site)
+repo = wikidata_site.data_repository()
+
+for item in generator:
+    itemdetails = item.get()
+
+    claim_list = itemdetails["claims"]["P140"]
+    for claim in claim_list:
+        trgt = claim.getTarget()
+        print("Claim for {} found.".format(trgt.id))
+        correct_page = pywikibot.ItemPage(repo, "Q9592", 0)
+        claim.changeTarget(correct_page)
