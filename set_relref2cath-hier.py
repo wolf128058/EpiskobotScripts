f7e65e6ffdfd4284b9d1d527b8b7271a975d1621
Jonas Hess <jonas.hess@mailbox.org>
2019-07-25 23:34:22 +0200

    +- get ready fpr python3

--- set_relref2cath-hier.py
+++ set_relref2cath-hier.py
@@ -37,24 +37,23 @@ for item in generator:
 
     for cathids in claim_list_cathid:
         mycathid = cathids.getTarget()
-        print '>> Catholic-Hierarchy-Id: ' + mycathid
+        print(('>> Catholic-Hierarchy-Id: ' + mycathid))
 
     for rel_claim in claim_list_religion:
         trgt = rel_claim.getTarget()
-        print '-- Claim for {} found.'.format(trgt.id)
+        print(('-- Claim for {} found.'.format(trgt.id)))
         if trgt.id == 'Q9592':
 
             if changed == False:
-                source_claim = pywikibot.Claim(repo, u'P1047')
+                source_claim = pywikibot.Claim(repo, 'P1047')
                 source_claim.setTarget(mycathid)
                 rel_claim_sources = rel_claim.getSources()
                 if len(rel_claim_sources) == 0:
                     r = requests.head(
                         'http://www.catholic-hierarchy.org/bishop/b' + mycathid + '.html')
                     if r.status_code == 200:
-                        print('-- Status 200 received for: ' +
-                              'http://www.catholic-hierarchy.org/bishop/b' + mycathid + '.html')
+                        print('-- Status 200 received for: ' + 'http://www.catholic-hierarchy.org/bishop/b' + mycathid + '.html')
                         rel_claim.addSources(
-                            [source_claim], summary=u'add catholic-hierarchy as source for religion')
+                            [source_claim], summary='add catholic-hierarchy as source for religion')
                 changed = True
 print('Done!')
