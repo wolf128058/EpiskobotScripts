82bfce83f0b71735bbfcff439f66866f345141b9
Jonas Hess <jonas.hess@mailbox.org>
2019-08-12 16:50:52 +0200

    +- exception handling and more regex for check_prieststart_...

--- check_prieststart_on_chorg.py
+++ check_prieststart_on_chorg.py
@@ -7,9 +7,13 @@ from pywikibot import pagegenerators as pg
 import lxml.html
 import re
 import requests
+import json
 
 import datetime
 from datetime import datetime
+
+from pprint import pprint
+
 
 wikidata_site = pywikibot.Site('wikidata', 'wikidata')
 QUERY_WITHOUT_START = """
@@ -20,15 +24,17 @@ SELECT ?item ?itemLabel ?birthLabel ?cathid WHERE {
   OPTIONAL { ?item wdt:P569 ?birth. }
   FILTER(NOT EXISTS { ?job pq:P580 ?start. })
   FILTER(EXISTS { ?item p:P1047 ?statement. })
-  FILTER((YEAR(?birth)) >= 0 )
+  FILTER((YEAR(?birth)) >= 1584 )
   SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],de,en". }
 }
-ORDER BY DESC (?birthLabel)
-LIMIT 1000
+ORDER BY RAND()
+# ORDER BY (?birthLabel)
+LIMIT 5000
 """
 path4qs = 'log_quick_prieststarts.txt'
 generator = pg.WikidataSPARQLPageGenerator(QUERY_WITHOUT_START, site=wikidata_site)
 item_properties = ''
+count_props = 0
 
 
 for item in generator:
@@ -69,6 +75,9 @@ for item in generator:
 
     claim_list_cathid = mywd['claims']['P1047']
     for cathids in claim_list_cathid:
+        data_found = False
+        priest_tr_circa = []
+        priest_tr = []
         mycathid = cathids.getTarget()
         print '-- Catholic-Hierarchy-Id: ' + mycathid
         chorgurl = 'http://www.catholic-hierarchy.org/bishop/b' + mycathid + '.html'
@@ -79,37 +88,87 @@ for item in generator:
             print '### HTTP-ERROR ON cath-id: ' + chorgurl
             continue
 
-        priest_tr = re.findall(r"\<tr\>\<td[^\>]+\>(.*)\<\/td\>\<td\>.*\<\/td\>\<td\>Ordained Priest\<\/td\>.*\<\/tr\>", r.content)
+        try:
+            priest_tr = re.findall(r"\<tr\>\<td[^\>]+\>(.*)\<\/td\>\<td\>.*\<\/td\>\<td\>Ordained Priest\<\/td\>.*\<\/tr\>", r.content)
+        except:
+            priest_tr = []
+
         if len(priest_tr) > 0:
             print '-- Priest-Info: ' + priest_tr[0]
             priest_tr = re.sub(r'\<[^\<]+>', '', priest_tr[0])
-            priest_tr_circa = re.findall(r"(.*)&sup[0-9];", priest_tr.strip())
+            priest_tr_circa = re.findall(r"(.*)&", priest_tr.strip())
         else:
             print '-- No Priest-Ordination Data in table found. I skip.'
-            continue
 
-        if len(priest_tr_circa) == 0:
-            prieststart_datetime = datetime.strptime(priest_tr, '%d %b %Y')
+        if len(priest_tr_circa) == 0 and len(priest_tr) > 0:
+            print '-- There is nice data as precise data.'
 
             try:
-                prieststart_datetime = datetime.strptime(priest_tr, '%d %b %Y')
-                item_properties += "\n" + mywd_id + "\tP106\tQ250867\tP580\t+" + prieststart_datetime.isoformat() + 'Z/11'
+                if data_found == False:
+                    prieststart_datetime = datetime.strptime(priest_tr.strip(), '%d %b %Y')
+                    item_properties += "\n" + mywd_id + "\tP106\tQ250867\tP580\t+" + prieststart_datetime.isoformat() + 'Z/11'
+                    count_props += 1
+                    data_found = True
             except:
-                print '- No valid startdate (precision day) found: "' + priest_tr + '"'
-                try:
-                    prieststart_datetime = datetime.strptime(priest_tr, '%b %Y')
+                if data_found == False:
+                    print '- No valid startdate (precision day) found: "' + priest_tr + '"'
+
+            try:
+                if data_found == False:
+                    prieststart_datetime = datetime.strptime(priest_tr.strip(), '%b %Y')
                     item_properties += "\n" + mywd_id + "\tP106\tQ250867\tP580\t+" + prieststart_datetime.isoformat() + 'Z/10'
-                except:
+                    count_props += 1
+                    data_found = True
+            except:
+                if data_found == False:
                     print '- No valid startdate (precision month) found: "' + priest_tr + '"'
-                    try:
-                        prieststart_datetime = datetime.strptime(priest_tr, '%Y')
-                        item_properties += "\n" + mywd_id + "\tP106\tQ250867\tP580\t+" + prieststart_datetime.isoformat() + 'Z/9'
-                    except:
-                        print '- No valid startdate (precision year) found: "' + priest_tr + '"'
 
+            try:
+                if data_found == False:
+                    prieststart_datetime = datetime.strptime(priest_tr.strip(), '%Y')
+                    item_properties += "\n" + mywd_id + "\tP106\tQ250867\tP580\t+" + prieststart_datetime.isoformat() + 'Z/9'
+                    count_props += 1
+            except:
+                if data_found == False:
+                    print '- No valid startdate (precision year) found:Francesco Saverio Dracopoli "' + priest_tr + '"'
 
-fq = open(path4qs, "a")
-fq.write(item_properties)
-fq.close()
+        elif(len(priest_tr_circa) > 0 and data_found == False):
+            print '-- But there is nice data as circa data.'
+
+            try:
+                if data_found == False:
+                    prieststart_datetime = datetime.strptime(priest_tr_circa[0].strip(), '%d %b %Y')
+                    item_properties += "\n" + mywd_id + "\tP106\tQ250867\tP580\t+" + prieststart_datetime.isoformat() + 'Z/11' + "\tP1480\tQ5727902"
+                    count_props += 1
+                    data_found = True
+            except:
+                if data_found == False:
+                    print '- No valid ~ startdate (precision day) found: "' + priest_tr_circa[0].strip() + '"'
+
+            try:
+                if data_found == False:
+                    prieststart_datetime = datetime.strptime(priest_tr_circa[0].strip(), '%b %Y')
+                    item_properties += "\n" + mywd_id + "\tP106\tQ250867\tP580\t+" + prieststart_datetime.isoformat() + 'Z/10' + "\tP1480\tQ5727902"
+                    count_props += 1
+                    data_found = True
+            except:
+                if data_found == False:
+                    print '- No valid ~ startdate (precision month) found: "' + priest_tr_circa[0].strip() + '"'
+
+            try:
+                if data_found == False:
+                    prieststart_datetime = datetime.strptime(priest_tr_circa[0].strip(), '%Y')
+                    item_properties += "\n" + mywd_id + "\tP106\tQ250867\tP580\t+" + prieststart_datetime.isoformat() + 'Z/9' + "\tP1480\tQ5727902"
+                    count_props += 1
+                    data_found = True
+            except:
+                if data_found == False:
+                    print '- No valid ~ startdate (precision day) found: "' + priest_tr_circa[0].strip() + '"'
+
+    if(count_props % 10 == 0):
+        fq = open(path4qs, "a")
+        fq.write(item_properties)
+        fq.close()
+        item_properties = ''
 
 print('Done!' + "\n")
