86db84c319e04783b19abb56c7bcc85d588fab6c
Jonas Hess <jonas.hess@mailbox.org>
2019-09-28 00:41:14 +0200

    ++ check_bishopstart_on_chorg.py got an progressbar

--- check_bishopstart_on_chorg.py
+++ check_bishopstart_on_chorg.py
@@ -4,12 +4,13 @@
 import pywikibot
 from pywikibot import pagegenerators as pg
 
+import datetime
+from datetime import datetime
+
 import lxml.html
 import re
 import requests
-
-import datetime
-from datetime import datetime
+import progressbar
 
 
 wikidata_site = pywikibot.Site('wikidata', 'wikidata')
@@ -29,119 +30,120 @@ LIMIT 5000
 """
 path4qs = 'log_quick_bishopstarts.txt'
 generator = pg.WikidataSPARQLPageGenerator(QUERY_WITHOUT_START, site=wikidata_site)
+generator = list(generator)
 item_properties = ''
 count_props = 0
 
+with progressbar.ProgressBar(max_value=len(generator), redirect_stdout=True) as bar:
+    for index, item in enumerate(generator):
+        mywd = item.get()
+        mywd_id = item.id
+        print("\n" + '>> Checking WD-ID: ' + mywd_id)
+        print('-- WD-URL: https://www.wikidata.org/wiki/' + mywd_id)
 
-for item in generator:
-    mywd = item.get()
-    mywd_id = item.id
-    print("\n" + '>> Checking WD-ID: ' + mywd_id)
-    print('-- WD-URL: https://www.wikidata.org/wiki/' + mywd_id)
+        claim_list_pos = {}
+        my_cathbishop_claim = {}
+        count_bishoppos = 0
+        try:
+            claim_list_pos = mywd['claims']['P39']
+            for pos_item in claim_list_pos:
+                my_pos_data = pos_item.getTarget()
+                if my_pos_data.id == 'Q611644':
+                    count_bishoppos += 1
+                    my_cathbishop_claim = my_pos_data
 
-    claim_list_pos = {}
-    my_cathbishop_claim = {}
-    count_bishoppos = 0
-    try:
-        claim_list_pos = mywd['claims']['P39']
-        for pos_item in claim_list_pos:
-            my_pos_data = pos_item.getTarget()
-            if my_pos_data.id == 'Q611644':
-                count_bishoppos += 1
-                my_cathbishop_claim = my_pos_data
+            print('-- Found {} Position-Claims for Catholic Bishop'.format(count_bishoppos))
+        except:
+            print('-- No Position for Catholic-Bishop found.')
 
-        print('-- Found {} Position-Claims for Catholic Bishop'.format(count_bishoppos))
-    except:
-        print('-- No Position for Catholic-Bishop found.')
-
-    if(count_bishoppos < 1):
-        print('-- {} Position-Claims for Catholic Bishop is to less. I skip.'.format(count_bishoppos))
-        continue
-    if(count_bishoppos > 1):
-        print('-- {} Position-Claims for Catholic Bishop is to much. I skip.'.format(count_bishoppos))
-        continue
-
-    try:
-        my_cathbishop_claim_start = my_cathbishop_claim['claims']['P580']
-        print('-- Sorry, but we have a startdate set already. I skip.')
-        continue
-
-    except:
-        print('-- No Startdate set. (yet...)')
-
-    claim_list_cathid = mywd['claims']['P1047']
-    for cathids in claim_list_cathid:
-        mycathid = cathids.getTarget()
-        print('-- Catholic-Hierarchy-Id: ' + mycathid)
-        chorgurl = 'http://www.catholic-hierarchy.org/bishop/b' + mycathid + '.html'
-        print('-- Catholic-Hierarchy-URL: ' + chorgurl)
-
-        r = requests.get(chorgurl)
-        if r.status_code != 200:
-            print('### HTTP-ERROR ON cath-id: ' + chorgurl)
+        if(count_bishoppos < 1):
+            print('-- {} Position-Claims for Catholic Bishop is to less. I skip.'.format(count_bishoppos))
+            continue
+        if(count_bishoppos > 1):
+            print('-- {} Position-Claims for Catholic Bishop is to much. I skip.'.format(count_bishoppos))
             continue
 
-        bishop_tr = re.findall(b'<tr><td[^>]+>(.*)</td><td>.*</td><td>Ordained Bishop</td>.*</tr>', r.content)
-        if len(bishop_tr) > 0:
-            print('-- Bishop-Info: ' + bishop_tr[0].decode('utf-8'))
-
-            bishop_tr_clean = ''
-            if(isinstance(bishop_tr[0].decode('utf-8'), str)):
-                bishop_tr_clean = re.sub(r'\<[^\<]+\>', '', bishop_tr[0].decode('utf-8'))
-            if(isinstance(bishop_tr[0], str)):
-                bishop_tr_clean = re.sub(r'\<[^\<]+\>', '', bishop_tr[0])
-
-            print('-- Bishop-Info-Clean: ' + bishop_tr_clean)
-
-            bishop_tr_circa = re.findall(r'(.*)&', bishop_tr_clean)
-        else:
-            print('-- No Bishop-Ordination Data in table found. I skip.')
+        try:
+            my_cathbishop_claim_start = my_cathbishop_claim['claims']['P580']
+            print('-- Sorry, but we have a startdate set already. I skip.')
             continue
 
-        if len(bishop_tr_circa) == 0:
+        except:
+            print('-- No Startdate set. (yet...)')
 
-            try:
-                bishopstart_datetime = datetime.strptime(bishop_tr_clean.strip(), '%d %b %Y')
-                item_properties += "\n" + mywd_id + "\tP39\tQ611644\tP580\t+" + bishopstart_datetime.isoformat() + 'Z/11' + "\tS1047\t\"" + mycathid + "\"\t"
-                count_props += 1
-            except:
-                print('- No valid startdate (precision day) found: "' + bishop_tr_clean + '"')
+        claim_list_cathid = mywd['claims']['P1047']
+        for cathids in claim_list_cathid:
+            mycathid = cathids.getTarget()
+            print('-- Catholic-Hierarchy-Id: ' + mycathid)
+            chorgurl = 'http://www.catholic-hierarchy.org/bishop/b' + mycathid + '.html'
+            print('-- Catholic-Hierarchy-URL: ' + chorgurl)
+
+            r = requests.get(chorgurl)
+            if r.status_code != 200:
+                print('### HTTP-ERROR ON cath-id: ' + chorgurl)
+                continue
+
+            bishop_tr = re.findall(b'<tr><td[^>]+>(.*)</td><td>.*</td><td>Ordained Bishop</td>.*</tr>', r.content)
+            if len(bishop_tr) > 0:
+                print('-- Bishop-Info: ' + bishop_tr[0].decode('utf-8'))
+
+                bishop_tr_clean = ''
+                if(isinstance(bishop_tr[0].decode('utf-8'), str)):
+                    bishop_tr_clean = re.sub(r'\<[^\<]+\>', '', bishop_tr[0].decode('utf-8'))
+                if(isinstance(bishop_tr[0], str)):
+                    bishop_tr_clean = re.sub(r'\<[^\<]+\>', '', bishop_tr[0])
+
+                print('-- Bishop-Info-Clean: ' + bishop_tr_clean)
+
+                bishop_tr_circa = re.findall(r'(.*)&', bishop_tr_clean)
+            else:
+                print('-- No Bishop-Ordination Data in table found. I skip.')
+                continue
+
+            if len(bishop_tr_circa) == 0:
+
                 try:
-                    bishopstart_datetime = datetime.strptime(bishop_tr_clean.strip(), '%b %Y')
-                    item_properties += "\n" + mywd_id + "\tP39\tQ611644\tP580\t+" + bishopstart_datetime.isoformat() + 'Z/10' + "\tS1047\t\"" + mycathid + "\"\t"
+                    bishopstart_datetime = datetime.strptime(bishop_tr_clean.strip(), '%d %b %Y')
+                    item_properties += "\n" + mywd_id + "\tP39\tQ611644\tP580\t+" + bishopstart_datetime.isoformat() + 'Z/11' + "\tS1047\t\"" + mycathid + "\"\t"
                     count_props += 1
                 except:
-                    print('- No valid startdate (precision month) found: "' + bishop_tr_clean + '"')
+                    print('- No valid startdate (precision day) found: "' + bishop_tr_clean + '"')
                     try:
-                        bishopstart_datetime = datetime.strptime(bishop_tr_clean.strip(), '%Y')
-                        item_properties += "\n" + mywd_id + "\tP39\tQ611644\tP580\t+" + bishopstart_datetime.isoformat() + 'Z/9' + "\tS1047\t\"" + mycathid + "\"\t"
+                        bishopstart_datetime = datetime.strptime(bishop_tr_clean.strip(), '%b %Y')
+                        item_properties += "\n" + mywd_id + "\tP39\tQ611644\tP580\t+" + bishopstart_datetime.isoformat() + 'Z/10' + "\tS1047\t\"" + mycathid + "\"\t"
                         count_props += 1
                     except:
-                        print('- No valid startdate (precision year) found: "' + priest_tr + '"')
+                        print('- No valid startdate (precision month) found: "' + bishop_tr_clean + '"')
+                        try:
+                            bishopstart_datetime = datetime.strptime(bishop_tr_clean.strip(), '%Y')
+                            item_properties += "\n" + mywd_id + "\tP39\tQ611644\tP580\t+" + bishopstart_datetime.isoformat() + 'Z/9' + "\tS1047\t\"" + mycathid + "\"\t"
+                            count_props += 1
+                        except:
+                            print('- No valid startdate (precision year) found: "' + priest_tr + '"')
 
-        else:
-            try:
-                bishopstart_datetime = datetime.strptime(bishop_tr_circa[0].strip(), '%d %b %Y')
-                item_properties += "\n" + mywd_id + "\tP39\tQ611644\tP580\t+" + bishopstart_datetime.isoformat() + 'Z/11' + "\tP1480\tQ5727902" + "\tS1047\t\"" + mycathid + "\"\t"
-                count_props += 1
-            except:
-                print('- No valid ~startdate (precision day) found: "' + bishop_tr_circa[0].strip() + '"')
+            else:
                 try:
-                    bishopstart_datetime = datetime.strptime(bishop_tr_circa[0].strip(), '%b %Y')
-                    item_properties += "\n" + mywd_id + "\tP39\tQ611644\tP580\t+" + bishopstart_datetime.isoformat() + 'Z/10' + "\tP1480\tQ5727902" + "\tS1047\t\"" + mycathid + "\"\t"
+                    bishopstart_datetime = datetime.strptime(bishop_tr_circa[0].strip(), '%d %b %Y')
+                    item_properties += "\n" + mywd_id + "\tP39\tQ611644\tP580\t+" + bishopstart_datetime.isoformat() + 'Z/11' + "\tP1480\tQ5727902" + "\tS1047\t\"" + mycathid + "\"\t"
                     count_props += 1
                 except:
-                    print('- No valid ~startdate (precision month) found: "' + bishop_tr_circa[0].strip() + '"')
+                    print('- No valid ~startdate (precision day) found: "' + bishop_tr_circa[0].strip() + '"')
                     try:
-                        bishopstart_datetime = datetime.strptime(bishop_tr_circa[0].strip(), '%Y')
-                        item_properties += "\n" + mywd_id + "\tP39\tQ611644\tP580\t+" + bishopstart_datetime.isoformat() + 'Z/9' + "\tP1480\tQ5727902" + "\tS1047\t\"" + mycathid + "\"\t"
+                        bishopstart_datetime = datetime.strptime(bishop_tr_circa[0].strip(), '%b %Y')
+                        item_properties += "\n" + mywd_id + "\tP39\tQ611644\tP580\t+" + bishopstart_datetime.isoformat() + 'Z/10' + "\tP1480\tQ5727902" + "\tS1047\t\"" + mycathid + "\"\t"
                         count_props += 1
                     except:
-                        print('- No valid ~startdate (precision year) found: "' + bishop_tr_circa[0].strip() + '"')
+                        print('- No valid ~startdate (precision month) found: "' + bishop_tr_circa[0].strip() + '"')
+                        try:
+                            bishopstart_datetime = datetime.strptime(bishop_tr_circa[0].strip(), '%Y')
+                            item_properties += "\n" + mywd_id + "\tP39\tQ611644\tP580\t+" + bishopstart_datetime.isoformat() + 'Z/9' + "\tP1480\tQ5727902" + "\tS1047\t\"" + mycathid + "\"\t"
+                            count_props += 1
+                        except:
+                            print('- No valid ~startdate (precision year) found: "' + bishop_tr_circa[0].strip() + '"')
+        bar.update(index)
+    fq = open(path4qs, "a")
+    fq.write(item_properties)
+    fq.close()
+    item_properties = ''
 
-        fq = open(path4qs, "a")
-        fq.write(item_properties)
-        fq.close()
-        item_properties = ''
-
-print(('Done!' + "\n"))
+print('Done!' + "\n")
