6343104690bd99a828f3fc794e30ea860b0bb17c
Jonas Hess <jonas.hess@mailbox.org>
2019-10-03 20:47:45 +0200

    +- migrated find_firstnames.py to python 3 (+ progressbar)

--- find_firstnames.py
+++ find_firstnames.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
 import pywikibot
@@ -9,6 +9,8 @@ from os import system
 
 import re
 import requests
+
+import progressbar
 
 
 def sparql_namesearch(firstname):
@@ -53,68 +55,71 @@ SELECT ?item ?itemLabel ?cathiLabel WHERE {
 wikidata_site = pywikibot.Site('wikidata', 'wikidata')
 
 generator = pg.WikidataSPARQLPageGenerator(QUERY, site=wikidata_site)
+generator = list(generator)
+
 repo = wikidata_site.data_repository()
 
 Names2Items = dict()
 NoNames = list()
 
 counter = 0
+with progressbar.ProgressBar(max_value=len(generator), redirect_stdout=True) as bar:
+    for index, item in enumerate(generator):
+        #    if counter >= 1000:
+        #        break
+        changed = False
+        item.get()
+        mylabel = ''
+        myid = item.id
+        print('>>> Examining: ' + item.id)
+        print('--- URL: https://www.wikidata.org/wiki/' + item.id)
+        if 'de' in item.labels and mylabel == '':
+            mylabel = item.labels['de']
+        if 'en' in item.labels and mylabel == '':
+            mylabel = item.labels['en']
+        if 'it' in item.labels and mylabel == '':
+            mylabel = item.labels['it']
+        if 'nl' in item.labels and mylabel == '':
+            mylabel = item.labels['nl']
+        if 'ast' in item.labels and mylabel == '':
+            mylabel = item.labels['ast']
+        if 'sl' in item.labels and mylabel == '':
+            mylabel = item.labels['sl']
+        print('--- Label:"' + mylabel + '"')
 
-for item in generator:
-    if counter >= 1000:
-        break
-    changed = False
-    item.get()
-    mylabel = ''
-    myid = item.id
-    print '>>> Examining: ' + item.id
-    print '--- URL: https://www.wikidata.org/wiki/' + item.id
-    if 'de' in item.labels and mylabel == '':
-        mylabel = item.labels['de']
-    if 'en' in item.labels and mylabel == '':
-        mylabel = item.labels['en']
-    if 'it' in item.labels and mylabel == '':
-        mylabel = item.labels['it']
-    if 'nl' in item.labels and mylabel == '':
-        mylabel = item.labels['nl']
-    if 'ast' in item.labels and mylabel == '':
-        mylabel = item.labels['ast']
-    if 'sl' in item.labels and mylabel == '':
-        mylabel = item.labels['sl']
-    print '--- Label:"' + mylabel + '"'
+        if mylabel == '':
+            print('!!! empty label. i skip.' + "\n")
+            continue
 
-    if mylabel == '':
-        print '!!! empty label. i skip.' + "\n"
-        continue
+        words = mylabel.split()
+        firstname = words[0]
+        firstname = firstname.encode('utf8')
 
-    words = mylabel.split()
-    firstname = words[0]
-    firstname = firstname.encode('utf8')
-
-    if firstname in Names2Items:
-        print '--- I know "' + firstname + '" is a first name.'
-        fq = open(path4qs, "a")
-        fq.write("\n" + myid + "\tP735\t" + Names2Items[firstname])
-        counter += 1
-        fq.close()
-    elif firstname in NoNames:
-        print '--- I know "' + firstname + '" is NOT A KNOWN first name.'
-    elif firstname not in NoNames:
-        print '--- I guess "' + words[0] + '" could be a first name.'
-        FN_SPARQL = sparql_namesearch(firstname)
-        subgenerator_fn = pg.WikidataSPARQLPageGenerator(FN_SPARQL, site=wikidata_site)
-        match = False
-        for item in subgenerator_fn:
-            match = True
-            mynameitem = item.get()
-            print '### Male firstname found: "' + words[0] + '" is ' + item.id
-            Names2Items[firstname] = item.id
+        if firstname in Names2Items:
+            print('--- I know "' + firstname.decode('utf-8') + '" is a first name.')
             fq = open(path4qs, "a")
-            fq.write("\n" + myid + "\tP735\t" + item.id)
+            fq.write("\n" + myid + "\tP735\t" + Names2Items[firstname])
             counter += 1
             fq.close()
-        if match == False:
-            NoNames.append(firstname)
-    print "\n"
+        elif firstname in NoNames:
+            print('--- I know "' + firstname.decode('utf-8') + '" is NOT A KNOWN first name.')
+        elif firstname not in NoNames:
+            print('--- I guess "' + words[0] + '" could be a first name.')
+            FN_SPARQL = sparql_namesearch(firstname.decode('utf-8'))
+            subgenerator_fn = pg.WikidataSPARQLPageGenerator(FN_SPARQL, site=wikidata_site)
+            match = False
+            for item in subgenerator_fn:
+                match = True
+                mynameitem = item.get()
+                print('### Male firstname found: "' + words[0] + '" is ' + item.id)
+                Names2Items[firstname] = item.id
+                fq = open(path4qs, "a")
+                fq.write("\n" + myid + "\tP735\t" + item.id)
+                counter += 1
+                fq.close()
+            if match == False:
+                NoNames.append(firstname)
+        print("\n")
+        bar.update(index)
 
 print('Done!')
