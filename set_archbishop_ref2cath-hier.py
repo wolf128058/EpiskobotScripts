444cf3a31be5c7767fc1b51fc0a7f26b176f54f5
Jonas Hess <jonas.hess@mailbox.org>
2019-10-13 03:42:48 +0200

    +- set_archbishop_ref2cath-hier.py: migrated to python3 added shuffle and progressbar

--- set_archbishop_ref2cath-hier.py
+++ set_archbishop_ref2cath-hier.py
@@ -1,63 +1,71 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
+
+
+from random import shuffle
+
+import progressbar
 
 import pywikibot
 from pywikibot import pagegenerators as pg
 
 import lxml.html
 
-QUERY = """
+QUERY = '''
 SELECT ?item ?itemLabel ?religion ?cathid ?rel2sub WHERE {
   ?item wdt:P39 wd:Q48629921;
-    wdt:P140 wd:Q9592, ?religion;
     wdt:P1047 ?cathid.
-  OPTIONAL { ?item wdt:P569 ?birth. }
   FILTER(NOT EXISTS {
     ?item p:P39 _:b80.
     _:b80 ps:P39 ?relsub2;
       prov:wasDerivedFrom _:b30.
   })
-  SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],de,en". }
 }
-ORDER BY DESC (?birth)
-LIMIT 5000
-"""
+'''
 
 wikidata_site = pywikibot.Site('wikidata', 'wikidata')
 
-# print(QUERY)
-
 generator = pg.WikidataSPARQLPageGenerator(QUERY, site=wikidata_site)
+generator = list(generator)
+shuffle(generator)
+
 repo = wikidata_site.data_repository()
 
-for item in generator:
-    itemdetails = item.get()
-    mycathid = ''
-    changed = False
+with progressbar.ProgressBar(max_value=len(generator), redirect_stdout=True) as bar:
+    bar.update(0)
+    for index, item in enumerate(generator):
+        itemdetails = item.get()
+        mycathid = ''
+        mywd_id = item.id
+        print("\n" + '>> Checking WD-ID: ' + mywd_id)
+        print('-- WD-URL: https://www.wikidata.org/wiki/' + mywd_id)
 
-    claim_list_position = itemdetails['claims']['P39']
-    claim_list_cathid = itemdetails['claims']['P1047']
+        claim_list_position = itemdetails['claims']['P39']
+        claim_list_cathid = itemdetails['claims']['P1047']
 
-    for cathids in claim_list_cathid:
-        mycathid = cathids.getTarget()
-        print '>> Catholic-Hierarchy-Id: ' + mycathid
+        for cathids in claim_list_cathid:
+            mycathid = cathids.getTarget()
+            print('>> Catholic-Hierarchy-Id: ' + mycathid)
+            print('>> URL: https://www.wikidata.org/wiki/' + item.id)
 
-    for rel_claim in claim_list_position:
-        trgt = rel_claim.getTarget()
-        print '-- Claim for {} found.'.format(trgt.id)
-        if trgt.id == 'Q48629921':
+        for rel_claim in claim_list_position:
+            trgt = rel_claim.getTarget()
+            print('-- Claim for {} found.'.format(trgt.id))
+            if trgt.id == 'Q48629921':
 
-            if changed == False:
-                source_claim = pywikibot.Claim(repo, u'P1047')
+                source_claim = pywikibot.Claim(repo, 'P1047')
                 source_claim.setTarget(mycathid)
                 rel_claim_sources = rel_claim.getSources()
 
-                if len(rel_claim_sources) == 0:
+                if not rel_claim_sources:
                     t = lxml.html.parse('http://www.catholic-hierarchy.org/bishop/b' + mycathid + '.html')
                     mytitle = t.find(".//title").text
 
                     if mytitle.startswith('Archbishop'):
                         print('-- Title match: "' + mytitle + '"')
-                        rel_claim.addSources([source_claim], summary=u'add catholic-hierarchy as source for position catholic-archbishop')
-                changed = True
+                        rel_claim.addSources([source_claim], summary='add catholic-hierarchy as source for position catholic-archbishop')
+                    else:
+                        print('-- Not title match: "' + mytitle + '"')
+        bar.update(index)
+
 print('Done!')
