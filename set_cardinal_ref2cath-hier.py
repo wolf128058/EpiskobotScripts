b232b4a0527cb85652d058e665752eb48fee2c86
Jonas Hess <jonas.hess@mailbox.org>
2020-04-24 03:56:59 +0200

    ++ just pylint-config-stuff

--- set_cardinal_ref2cath-hier.py
+++ set_cardinal_ref2cath-hier.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-
+# pylint: disable=missing-docstring, line-too-long
 
 from random import shuffle
 
--- set_priest_ref2cath-hier.py
+++ set_priest_ref2cath-hier.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+# pylint: disable=missing-docstring, line-too-long
 
 import progressbar
 import re
