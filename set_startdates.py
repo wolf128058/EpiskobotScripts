be21b51a8121cbd23642c54d25c93d02e12e31e7
Jonas Hess <jonas.hess@mailbox.org>
2020-05-13 01:58:11 +0200

    ++ new features: receive position and diocese as parameters, handle different positions (arrays with positions), year-precision-date-alternative

--- set_startdates.py
+++ set_startdates.py
@@ -3,9 +3,13 @@
 # pylint: disable=missing-docstring, line-too-long, bare-except, invalid-name
 
 import re
+import os
+import random
 from random import shuffle
 
-import datetime
+import sys
+import getopt
+
 from datetime import datetime
 
 import requests
@@ -38,25 +42,72 @@ def requests_retry_session(
     return session
 
 
-def dioid2wd(dioid):
-    QUERY4DIOID = 'SELECT ?item ?itemLabel WHERE { ?item wdt:P1866 "' + dioid + '". }'
-    try:
-        for my_item in pg.WikidataSPARQLPageGenerator(QUERY4DIOID, site=pywikibot.Site('wikidata', 'wikidata')):
-            my_item.get(get_redirect=True)
-            print('--- WD-Item-4-Diocese found: ' + my_item.id)
-            return my_item.id
+diocese = False
+position = False
 
-    except:
-        print('!!! query for diocese failed.')
-        return False
+
+def main(argv):
+    opts, args = getopt.getopt(argv, "hd:p:", ["diocese=", "position="])
+    print(args)
+    ret = {}
+    for opt, arg in opts:
+        if opt == '-h':
+            print('set_dbishopstartdates.py -d <diocese> -p <position>')
+            sys.exit()
+        elif opt in ("-d", "--diocese"):
+            diocese = arg
+            if diocese.isnumeric():
+                diocese = 'Q' + diocese
+            ret['dio'] = diocese
+        elif opt in ("-p", "--position"):
+            position = arg
+            if position.isnumeric():
+                position = 'Q'.position
+            ret['pos'] = position
+    return ret
+
+
+if __name__ == "__main__":
+    mainret = main(sys.argv[1:])
+    if 'dio' in mainret:
+        diocese = mainret['dio']
+    if 'pos' in mainret:
+        position = mainret['pos']
+
+list_events = ['Appointed', 'Selected', 'Succeeded', 'Confirmed']
+list_pos = {}
+list_pos['Q1144278'] = ['Bishop', 'Archbishop \(Personal Title\)']
+list_pos['Q948657'] = ['Titular Bishop']
+list_pos['Q48629921'] = ['Archbishop']
+list_pos['Q50362553'] = ['Titular Archbishop']
+list_pos['Q75178'] = ['Auxiliary Bishop']
+list_pos['Q15253909'] = ['Vicar Apostolic']
+list_pos['Q1993358'] = ['Apostolic Administrator']
+list_pos['Q65924966'] = ['Bishop', 'Archbishop', 'Archbishop \(Personal Title\)']
+
+if position == False and diocese != False:
+    for single_pos in list_pos.keys():
+        os.system('python3 set_dbishopstartdates.py -d ' + diocese + ' -p' + single_pos)
+
+if not position in list_pos.keys():
+    print('!!! - ' + str(position) + ' is not a position this script is designed for.')
+    exit()
 
 
 QUERY = """
-SELECT ?item ?birthLabel WHERE {
+SELECT ?item WHERE {
   ?item wdt:P1047 ?cathi;
-    wdt:P39 wd:Q1144278.
+    wdt:P39 wd:""" + position + """.
 }
 """
+
+if diocese:
+    QUERY = """
+    SELECT ?item WHERE {
+    ?item wdt:P39 wd:""" + position + """;
+        p:P39 ?DBship.
+    ?DBship ps:P39 wd:""" + position + """;
+        pq:P708 wd:""" + diocese + ".}"
 
 wikidata_site = pywikibot.Site('wikidata', 'wikidata')
 repo = wikidata_site.data_repository()
@@ -67,6 +118,7 @@ generator = list(generator)
 print('>>> Starting with ' + str(len(generator)) + ' entries')
 shuffle(generator)
 
+my_succ_dio_id = False
 
 with progressbar.ProgressBar(max_value=len(generator), redirect_stdout=True) as bar:
     for index, item in enumerate(generator):
@@ -78,9 +130,14 @@ with progressbar.ProgressBar(max_value=len(generator), redirect_stdout=True) as 
         changed = False
 
         print('>> CHECKING: Wikidata-Item: https://www.wikidata.org/wiki/' + itemdetails_id)
-        claim_list_cathid = itemdetails['claims']['P1047']
+        try:
+            claim_list_cathid = itemdetails['claims']['P1047']
+        except:
+            print('-- Not Catholic-Hierarchy-Id found. I skip that one')
+            continue
         mycathid = claim_list_cathid[0].getTarget()
         if not mycathid:
+            print('-- Not Catholic-Hierarchy-Id found. I skip that one')
             continue
 
         l_dbishop = []
@@ -97,7 +154,7 @@ with progressbar.ProgressBar(max_value=len(generator), redirect_stdout=True) as 
         for single_pos in claim_list_currentpositions:
             trgt_pos = single_pos.getTarget()
             to_json = single_pos.toJSON()
-            if trgt_pos.id == 'Q1144278':
+            if trgt_pos.id == position:
                 a_qualis = single_pos.qualifiers
                 if 'P580' in a_qualis:
                     str_dio = ''
@@ -110,13 +167,14 @@ with progressbar.ProgressBar(max_value=len(generator), redirect_stdout=True) as 
                     dates_missing += 1
 
         if dates_missing == 0:
-            print('-- All Diocesan Bishop Positions have start-dates. I skip that one.')
+            print('-- All selected Positions have start-dates. I skip that one.')
             continue
 
         if l_dbishopships_wostart:
-            print('-- Found {} Job-Claims for diocesan bishopships without start-date'.format(len(l_dbishopships_wostart)))
+            print('-- Found {} Job-Claims for selected jobs without start-date'.format(
+                len(l_dbishopships_wostart)))
         else:
-            print('### No Dio-Bishop w/o startdate!')
+            print('### No selected position w/o startdate!')
             continue
 
         chorgurl = 'http://www.catholic-hierarchy.org/bishop/b' + mycathid + '.html'
@@ -158,8 +216,11 @@ with progressbar.ProgressBar(max_value=len(generator), redirect_stdout=True) as 
                 continue
 
             if len(dio_claim_list_chd_id) != 1:
+                if diocese != False and my_succ_dio_id:
+                    dio_claim_list_chd_id = my_succ_dio_id
+
+                dio_claim_list_chd_id = [random.choice(dio_claim_list_chd_id)]
                 print('### DioId-Claim-Check failed (not unique)!')
-                continue
 
             my_chdid = ''
             d_to_json = dio_claim_list_chd_id[0].toJSON()
@@ -173,50 +234,34 @@ with progressbar.ProgressBar(max_value=len(generator), redirect_stdout=True) as 
                 print('-- Pos for dio:' + my_chdid)
                 # and now check for start in this dio
 
-                bytes_regex = str.encode('<tr><td[^>]+>(.*)</td><td>.*</td><td>Appointed</td><td>Bishop of <a href="/diocese/d' + my_chdid + '.html">.*</td>.*</tr>')
-                bishop_tr = re.findall(bytes_regex, r.content)
-                if not bishop_tr or len(bishop_tr) != 1:
-                    bytes_regex = str.encode('<tr><td[^>]+>(.*)</td><td>Appointed</td><td>Bishop of <a href="/diocese/d' + my_chdid + '.html">.*</td>.*</tr>')
-                    bishop_tr = re.findall(bytes_regex, r.content)
-                if not bishop_tr or len(bishop_tr) != 1:
-                    bytes_regex = str.encode('<tr><td[^>]+>(.*)</td><td>.*</td><td>Selected</td><td>Bishop of <a href="/diocese/d' + my_chdid + '.html">.*</td>.*</tr>')
-                    bishop_tr = re.findall(bytes_regex, r.content)
-                if not bishop_tr or len(bishop_tr) != 1:
-                    bytes_regex = str.encode('<tr><td[^>]+>(.*)</td><td>Selected</td><td>Bishop of <a href="/diocese/d' + my_chdid + '.html">.*</td>.*</tr>')
-                    bishop_tr = re.findall(bytes_regex, r.content)
-                if not bishop_tr or len(bishop_tr) != 1:
-                    bytes_regex = str.encode('<tr><td[^>]+>(.*)</td><td>.*</td><td>Succeeded</td><td>Bishop of <a href="/diocese/d' + my_chdid + '.html">.*</td>.*</tr>')
-                    bishop_tr = re.findall(bytes_regex, r.content)
-                if not bishop_tr or len(bishop_tr) != 1:
-                    bytes_regex = str.encode('<tr><td[^>]+>(.*)</td><td>Succeeded</td><td>Bishop of <a href="/diocese/d' + my_chdid + '.html">.*</td>.*</tr>')
-                    bishop_tr = re.findall(bytes_regex, r.content)
-                if not bishop_tr or len(bishop_tr) != 1:
-                    bytes_regex = str.encode('<tr><td[^>]+>(.*)</td><td>.*</td><td>Appointed</td><td>Archbishop \(Personal Title\) of <a href="/diocese/d' + my_chdid + '.html">.*</td>.*</tr>')
-                    bishop_tr = re.findall(bytes_regex, r.content)
-                if not bishop_tr or len(bishop_tr) != 1:
-                    bytes_regex = str.encode('<tr><td[^>]+>(.*)</td><td>Appointed</td><td>Archbishop \(Personal Title\) of <a href="/diocese/d' + my_chdid + '.html">.*</td>.*</tr>')
-                    bishop_tr = re.findall(bytes_regex, r.content)
-                if not bishop_tr or len(bishop_tr) != 1:
-                    bytes_regex = str.encode('<tr><td[^>]+>(.*)</td><td>.*</td><td>Succeeded</td><td>Archbishop \(Personal Title\) of <a href="/diocese/d' + my_chdid + '.html">.*</td>.*</tr>')
-                    bishop_tr = re.findall(bytes_regex, r.content)
-                if not bishop_tr or len(bishop_tr) != 1:
-                    bytes_regex = str.encode('<tr><td[^>]+>(.*)</td><td>Succeeded</td><td>Archbishop \(Personal Title\) of <a href="/diocese/d' + my_chdid + '.html">.*</td>.*</tr>')
-                    bishop_tr = re.findall(bytes_regex, r.content)
+                bishop_tr = False
+                for pos_name in list_pos[position]:
+                    for event in list_events:
+                        if not bishop_tr or len(bishop_tr) != 1:
+                            bytes_regex = str.encode('<tr><td[^>]+>(.*)</td><td>.*</td><td>' + event + '</td><td>' + pos_name + ' of '
+                                                     '<a href="/diocese/d' + my_chdid + '.html">.*</td>.*</tr>')
+                            bishop_tr = re.findall(bytes_regex, r.content)
+                        if not bishop_tr or len(bishop_tr) != 1:
+                            bytes_regex = str.encode('<tr><td[^>]+>(.*)</td><td>' + event + '</td><td>' + pos_name + ' of '
+                                                     '<a href="/diocese/d' + my_chdid + '.html">.*</td>.*</tr>')
+                            bishop_tr = re.findall(bytes_regex, r.content)
 
                 if bishop_tr and len(bishop_tr) == 1:
-                    print('-- Bishop-Info: ' + bishop_tr[0].decode('utf-8'))
+                    print('-- Pos-Start-Info: ' + bishop_tr[0].decode('utf-8'))
 
                     bishop_tr_clean = ''
                     if isinstance(bishop_tr[0].decode('utf-8'), str):
                         bishop_tr_clean = re.sub(r'\<[^\<]+\>', '', bishop_tr[0].decode('utf-8'))
+                        bishop_tr_clean = bishop_tr_clean.strip()
                     if isinstance(bishop_tr[0], str):
                         bishop_tr_clean = re.sub(r'\<[^\<]+\>', '', bishop_tr[0])
+                        bishop_tr_clean = bishop_tr_clean.strip()
 
-                    print('-- Bishop-Info-Clean: ' + bishop_tr_clean)
+                    print('-- Pos-Info-Clean: ' + bishop_tr_clean)
 
                     bishop_tr_circa = re.findall(r'(.*)&', bishop_tr_clean)
                 else:
-                    print('-- No Bishop-Ordination Data in table found. I skip.')
+                    print('-- No Pos-Start Data in table found. I skip.')
                     continue
 
                 if not bishop_tr_circa:
@@ -224,7 +269,9 @@ with progressbar.ProgressBar(max_value=len(generator), redirect_stdout=True) as 
                         bishopstart_datetime = datetime.strptime(bishop_tr_clean, '%d %b %Y')
                         if bishopstart_datetime.year < 1584:
                             my_calendar_model = 'http://www.wikidata.org/entity/Q1985786'
-                        start_date4wd = pywikibot.WbTime(year=bishopstart_datetime.year, month=bishopstart_datetime.month, day=bishopstart_datetime.day, precision=11, before=0, after=0,
+                        start_date4wd = pywikibot.WbTime(year=bishopstart_datetime.year,
+                                                         month=bishopstart_datetime.month, day=bishopstart_datetime.day,
+                                                         precision=11, before=0, after=0,
                                                          timezone=0, calendarmodel=my_calendar_model)
                         date_found = True
 
@@ -234,7 +281,9 @@ with progressbar.ProgressBar(max_value=len(generator), redirect_stdout=True) as 
                             bishopstart_datetime = datetime.strptime(bishop_tr_clean, '%b %Y')
                             if bishopstart_datetime.year < 1584:
                                 my_calendar_model = 'http://www.wikidata.org/entity/Q1985786'
-                            start_date4wd = pywikibot.WbTime(year=bishopstart_datetime.year, month=bishopstart_datetime.month, day=1, precision=10, before=0, after=0,
+                            start_date4wd = pywikibot.WbTime(year=bishopstart_datetime.year,
+                                                             month=bishopstart_datetime.month, day=1, precision=10,
+                                                             before=0, after=0,
                                                              timezone=0, calendarmodel=my_calendar_model)
                             date_found = True
 
@@ -244,7 +293,8 @@ with progressbar.ProgressBar(max_value=len(generator), redirect_stdout=True) as 
                                 bishopstart_datetime = datetime.strptime(bishop_tr_clean, '%Y')
                                 if bishopstart_datetime.year < 1584:
                                     my_calendar_model = 'http://www.wikidata.org/entity/Q1985786'
-                                start_date4wd = pywikibot.WbTime(year=bishopstart_datetime.year, month=1, day=1, precision=9, before=0, after=0,
+                                start_date4wd = pywikibot.WbTime(year=bishopstart_datetime.year, month=1, day=1,
+                                                                 precision=9, before=0, after=0,
                                                                  timezone=0, calendarmodel=my_calendar_model)
                                 date_found = True
 
@@ -256,7 +306,9 @@ with progressbar.ProgressBar(max_value=len(generator), redirect_stdout=True) as 
                         bishopstart_datetime = datetime.strptime(bishop_tr_circa[0], '%d %b %Y')
                         if bishopstart_datetime.year < 1584:
                             my_calendar_model = 'http://www.wikidata.org/entity/Q1985786'
-                        start_date4wd = pywikibot.WbTime(year=bishopstart_datetime.year, month=bishopstart_datetime.month, day=bishopstart_datetime.day, precision=11, before=0, after=0,
+                        start_date4wd = pywikibot.WbTime(year=bishopstart_datetime.year,
+                                                         month=bishopstart_datetime.month, day=bishopstart_datetime.day,
+                                                         precision=11, before=0, after=0,
                                                          timezone=0, calendarmodel=my_calendar_model)
                         date_found = True
 
@@ -266,7 +318,9 @@ with progressbar.ProgressBar(max_value=len(generator), redirect_stdout=True) as 
                             bishopstart_datetime = datetime.strptime(bishop_tr_circa[0], '%b %Y')
                             if bishopstart_datetime.year < 1584:
                                 my_calendar_model = 'http://www.wikidata.org/entity/Q1985786'
-                            start_date4wd = pywikibot.WbTime(year=bishopstart_datetime.year, month=bishopstart_datetime.month, day=1, precision=10, before=0, after=0,
+                            start_date4wd = pywikibot.WbTime(year=bishopstart_datetime.year,
+                                                             month=bishopstart_datetime.month, day=1, precision=10,
+                                                             before=0, after=0,
                                                              timezone=0, calendarmodel=my_calendar_model)
                             date_found = True
 
@@ -276,13 +330,28 @@ with progressbar.ProgressBar(max_value=len(generator), redirect_stdout=True) as 
                                 bishopstart_datetime = datetime.strptime(bishop_tr_circa[0], '%Y')
                                 if bishopstart_datetime.year < 1584:
                                     my_calendar_model = 'http://www.wikidata.org/entity/Q1985786'
-                                start_date4wd = pywikibot.WbTime(year=bishopstart_datetime.year, month=1, day=1, precision=9, before=0, after=0, timezone=0, calendarmodel=my_calendar_model)
+                                start_date4wd = pywikibot.WbTime(year=bishopstart_datetime.year, month=1, day=1,
+                                                                 precision=9, before=0, after=0, timezone=0,
+                                                                 calendarmodel=my_calendar_model)
                                 date_found = True
 
                             except:
-                                print('- No valid ~startdate (precision year) found: "' + bishop_tr_circa[0] + '"')
+                                # print('- No valid ~startdate (precision year) found: "' + bishop_tr_circa[0] + '"')
+                                try:
+                                    if (bishop_tr_circa[0].strip().isnumeric()):
+                                        tmpyear = int(bishop_tr_circa[0].strip())
+                                        if tmpyear < 1584:
+                                            my_calendar_model = 'http://www.wikidata.org/entity/Q1985786'
+                                        start_date4wd = pywikibot.WbTime(year=tmpyear, month=1, day=1,
+                                                                         precision=9, before=0, after=0, timezone=0,
+                                                                         calendarmodel=my_calendar_model)
+                                        date_found = True
+                                except:
+                                    print('- No valid ~startdate (precision year) found: "' + bishop_tr_circa[0] + '"')
 
             if date_found:
+                if diocese != False:
+                    my_succ_dio_id = dio_claim_list_chd_id
                 print(start_date4wd)
                 claim_startdate.setTarget(start_date4wd)
                 attempts = 0
